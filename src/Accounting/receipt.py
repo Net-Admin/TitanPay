883f57a05b940d9df52f3ef727c2261259c8f624
Joseph Parise <medic_24@hotmail.com>
2017-05-30 00:10:42 -0400

    update

--- src/Accounting/receipt.py
+++ src/Accounting/receipt.py
@@ -3,17 +3,17 @@
 class Receipt:
 
     def __init__(self, date, sale_amt):
-        self.__date = date
-        self.__sale_amt = sale_amt
+        self.__date = date()
+        self.__sale_amt = sale_amt()
 
     def set_date(self, date):
-        self.__date = date
+        self.__date = date()
 
     def set_sale_amt(self, sale_amt):
-        self.__sale_amt = sale_amt
+        self.__sale_amt = sale_amt()
 
     def get_date(self, date):
-        self.__date = date
+        self.__date = date()
 
     def get_sale_amt(self, sale_amt):
-        self.__sale_amt = sale_amt
+        self.__sale_amt = sale_amt()
--- src/Accounting/timecard.py
+++ src/Accounting/timecard.py
@@ -3,24 +3,24 @@
 class TimeCard:
 
     def __init__(self, date, start_time, end_time):
-        self.__date = date
-        self.__start_time = start_time
-        self.__end_time = end_time
+        self.__date = date()
+        self.__start_time = start_time()
+        self.__end_time = end_time()
 
     def set_date(self, date):
-        self.__date = date
+        self.__date = date()
 
     def set_start_time(self, start_time):
-        self.__start_time = start_time
+        self.__start_time = start_time()
 
     def set_end_time(self, end_time):
-        self.__end_time = end_time
+        self.__end_time = end_time()
 
     def get_date(self, date):
-        self.__date = date
+        self.__date = date()
 
     def get_start_time(self, start_time):
-        self.__start_time = start_time
+        self.__start_time = start_time()
 
     def get_end_time(self, end_time):
-        self.__end_time = end_time
+        self.__end_time = end_time()
