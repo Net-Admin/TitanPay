60318b2513e37c08a9bd28c7ae4320fc8cfb9007
twilhite82 <twilhite82@users.noreply.github.com>
2017-05-30 23:37:00 -0400

    Update timecard.py

--- src/Accounting/timecard.py
+++ src/Accounting/timecard.py
@@ -7,4 +7,11 @@ class TimeCard:
         self.__start_time = start_time
         self.__end_time = end_time
 
-    
+    def calculate_daily_pay(self, rate):
+        hours_worked = end_time - start_time
+        if hours_worked > 8:
+            pay = (1.5 * rate * (hours_worked - 8)) + 8 * rate
+        else:
+            rate * hours_worked
+
+        return pay()      
