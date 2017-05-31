f3c94afca7408cf960361274c4b0819bcff0eac5
twilhite82 <twilhite82@users.noreply.github.com>
2017-05-30 23:21:18 -0400

    Update bankaccount.py

--- src/Accounting/bankaccount.py
+++ src/Accounting/bankaccount.py
@@ -7,11 +7,4 @@ class BankAccount:
         self.__routing_nmber = routing_number
         self.__account_id = account_id
 
-    def get_bank_name(self, bank_name):
-        self.__bank_name = bank_name
-
-    def get_start_time(self, routing_number):
-        self.__routing_nmber = routing_number
-
-    def get_account_id(self, account_id):
-        self.__account_id = account_id
+    
