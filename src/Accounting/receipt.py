2ad96a469639d178849de3625288e70b3e5ee681
Joseph Parise <jparis8@live.spcollege.edu>
2017-05-31 22:33:46 -0400

    Update receipt.py

--- src/Accounting/receipt.py
+++ src/Accounting/receipt.py
@@ -5,11 +5,4 @@ class Receipt:
     def __init__(self, date, sale_amt):
         self.__date = date
         self.__sale_amt = sale_amt
-        
-    def get_date(self):
-        return self.__date
-    
-    def get_sale_amt(self):
-        return self.__sale_amt
-
     
