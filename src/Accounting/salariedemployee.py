950061cb21e0eaa060da625184059f6cc05260f7
Joseph Parise <jparis8@live.spcollege.edu>
2017-05-31 22:37:12 -0400

    Update salariedemployee.py

--- src/Accounting/salariedemployee.py
+++ src/Accounting/salariedemployee.py
@@ -10,20 +10,8 @@ class SalariedEmployee:
         self.__commission_rate = commission_rate
         self.__weekly_dues = weekly_dues
 
-   def get_employee_id(self):
+    def get_employee_id(self):
         return self.__employee_id
 
-    def get_first_name(self):
-        return self.__first_name
-
-    def get_last_name(self):
-        return self.__last_name
-
-    def get_salary(self):
-        return self.__salary
-
-    def get_commission_rate (self):
-        return self.__commission_rate
-
-    def get_weekly_dues(self):
-        return self.__weekly_dues
+    def get_full_name(self):
+        return self.__last_name + "," self.__first_name
