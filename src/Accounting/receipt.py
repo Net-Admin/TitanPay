587a3de0cb309be2252075353abc56a6457c7728
Joseph Parise <medic_24@hotmail.com>
2017-06-04 17:43:52 -0400

    Update

--- src/Accounting/receipt.py
+++ src/Accounting/receipt.py
@@ -5,8 +5,3 @@ class Receipt:
     def __init__(self, date, sale_amt):
         self.__date = date
         self.__sale_amt = sale_amt
-<<<<<<< HEAD
-
-=======
->>>>>>> b391dcd843a91a5c6017fc927ecb8ab48456ee92
-    
