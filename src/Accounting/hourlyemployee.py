6d0ca8f60d6a99549bd97c9cf448aea5df4c9b76
Joseph Parise <medic_24@hotmail.com>
2017-06-06 00:00:46 -0400

    update
    Removed the two def get statements per professor's feedback

--- src/Accounting/hourlyemployee.py
+++ src/Accounting/hourlyemployee.py
@@ -3,9 +3,3 @@ class HourlyEmployee(Employee):
     def __init__(self, employee_id, first_name, last_name, hourly_rate, weekly_dues):
         Employee.__init__(self, employee_id, first_name, last_name, weekly_dues)
         self.__hourly_rate = hourly_rate
-
-    def get_employee_id(self):
-        return self.__employee_id
-
-    def get_full_name(self):
-        return self.__last_name + "," + self.__first_name
