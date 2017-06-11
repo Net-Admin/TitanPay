419652ee9426af35476c909b53a9596cbc6a7290
twilhite82 <twilhite82@users.noreply.github.com>
2017-06-11 18:01:49 -0400

    Create hourlyemployee.py

--- src/Accounting/hourlyemployee.py
+++ src/Accounting/hourlyemployee.py
@@ -6,3 +6,12 @@ class HourlyEmployee(Employee, TimeCard):
         self.__hourly_rate = hourly_rate
         self.__clockIn = ClockIn
         self.__clockOut = ClockOut
+        
+    def __clockin__(self, TimeCard):
+        time_card = TimeCard(self, date, time)
+        self.__timecard_history.append(time_card)
+        
+    def __clockout__(self, TimeCard):
+        get (TimeCard)
+        time_card = TimeCard(self, date, time)
+        
