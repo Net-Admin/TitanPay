ab6356e37335c1affe6737ab13b9d19d3faf8fbc
twilhite82 <twilhite82@users.noreply.github.com>
2017-06-11 16:58:16 -0400

    Create salariedemployee.py

--- src/Accounting/salariedemployee.py
+++ src/Accounting/salariedemployee.py
@@ -4,5 +4,6 @@ class SalariedEmployee (Employee):
 
     def __init__(self, employee_id, first_name, last_name, salary, commission_rate, weekly_dues):
         Employee.__init__(self, employee_id, first_name, last_name, weekly_dues)
+        Receipt.__init__(self, employee_id, first_name, last_name, reciept_amt)
         self.__salary = salary
         self.__commission_rate = commission_rate
