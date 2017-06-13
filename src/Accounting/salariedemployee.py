c9c0386249e782e00cc012bdeab4727e6d8047e2
Joseph Parise <medic_24@hotmail.com>
2017-06-12 23:46:17 -0400

    6/12/2017 update

--- src/Accounting/salariedemployee.py
+++ src/Accounting/salariedemployee.py
@@ -2,11 +2,12 @@
 
 class SalariedEmployee(Employee):
 
-    def __init__(self, employee_id, first_name, last_name, salary, commission_rate, weekly_dues):
+    def __init__(self, employee_id, first_name, last_name, salary, commission_rate, receipts, weekly_dues):
         Employee.__init__(self, employee_id, first_name, last_name, weekly_dues)
         Receipt.__init__(self, employee_id, first_name, last_name, reciept_amt)
         self.__salary = salary
         self.__commission_rate = commission_rate
+        self.__receipts = []
         
     def __makeSale(double_amt):
         reciept = Receipt(self, transaction_amt)
