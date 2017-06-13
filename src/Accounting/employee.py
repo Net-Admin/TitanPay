3084736cef3503b98c1b8f0c8b5ee9c5783d2a0c
Joseph Parise <medic_24@hotmail.com>
2017-06-12 23:43:31 -0400

    6/12/2017 Update

--- src/Accounting/employee.py
+++ src/Accounting/employee.py
@@ -1,8 +1,5 @@
 # This stores the Employee information
 
-    from datetime import datetime
-    from datetime import timedelta
-
 class Employee :
 
     def __init__(self, employee_id, first_name, last_name, weekly_dues, clock_in, clock_out, time_cards):
@@ -10,9 +7,7 @@ class Employee :
         self.__first_name = first_name
         self.__last_name = last_name
         self.__weekly_dues = weekly_dues
-        self.__clock_in = clockIn
-        self.__clock_out = clockOut
-        self.__time_cards = []
+
 
 
     def get_employee_id(self):
@@ -20,16 +15,3 @@ class Employee :
 
     def get_full_name(self):
         return self.__last_name + " ," + self.__first_name
-
-    def clockIn(self):
-        self.__clock_in = datetime.strftime('%m/%d/%Y %H:%M:%S')
-        tc = TimeCard()
-        self.__time_cards.append(tc)
-
-    def clockOut(self):
-        self.clock_out = datetime.strftime('%m/%d/%Y %H:%M:%S')
-        = str(datetime.strftime('%m/%d/%Y %H:%M:%S')) - timedelta(clockin)
-        for tc in self.__time_cards:
-            if tc.get_date() == current_dt:
-                tc.clockOut()
-
--- src/Accounting/hourlyemployee.py
+++ src/Accounting/hourlyemployee.py
@@ -1,24 +1,31 @@
-import datetime
+from datetime import datetime
+from datetime import timedelta
+
 class HourlyEmployee(Employee, TimeCard):
 
-    def __init__(self, employee_id, first_name, last_name, hourly_rate, weekly_dues, clockIn, clockOut):
+    def __init__(self, employee_id, first_name, last_name, hourly_rate, weekly_dues, clockin, clockout, timecards):
         Employee.__init__(self, employee_id, first_name, last_name, weekly_dues)
         TimeCard.__init__(self, date, start_time, end_time)
         self.__hourly_rate = hourly_rate
-        self.__clockIn = ClockIn
-        self.__clockOut = ClockOut
+        self.__clockin = clockIn
+        self.__clockout = clockOut
+        self.__timecards = []
         
     def __date_time(self,datetime):
         now = datetime.datetime.now()
         
-    def __clockin__(self, TimeCard):
-        time_card = TimeCard(self, now)
-        self.__timecard_history.append(time_card)
-        
-    def __clockout__(self, TimeCard):
-        get (TimeCard)
-        time_card = TimeCard(self, now)
-        
+    def clockIn(self):
+        self.__clock_in = datetime.strftime('%m/%d/%Y %H:%M:%S')
+        tc = TimeCard()
+        self.__time_cards.append(tc)
+
+    def clockOut(self):
+        self.clock_out = datetime.strftime('%m/%d/%Y %H:%M:%S')
+        = str(datetime.strftime('%m/%d/%Y %H:%M:%S')) - timedelta(clockin)
+        for tc in self.__timecards:
+            if tc.get_date() == current_dt:
+                tc.clockOut()
+
     def __calculate_pay__(self, time_card, hourly_rate):
         pay = time_card * hourly_rate
         time_card = 0
