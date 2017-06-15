8698fb29d81ccefbfa04c31e5db17768061acd15
twilhite82 <twilhite82@users.noreply.github.com>
2017-06-14 21:58:02 -0400

    Create receipt.py

--- src/Accounting/receipt.py
+++ src/Accounting/receipt.py
@@ -5,3 +5,9 @@ class Receipt:
     def __init__(self, date, sale_amt):
         self.__date = date
         self.__sale_amt = sale_amt
+        
+    def __commission__(self):
+        self.__receipt = receipt
+        self.__commission_rate = commission_rate
+        self.__calculate_commission = receipt * commission_rate
+        
