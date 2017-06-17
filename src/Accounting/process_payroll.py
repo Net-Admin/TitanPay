5236862b40f3731ed28ccca69a85ee559d69e11b
Joseph Parise <medic_24@hotmail.com>
2017-06-17 17:59:41 -0400

    5A update v1

---
+++ src/Accounting/process_payroll.py
@@ -0,0 +1,31 @@
+import tkinter
+
+class ProcessingPayroll:
+
+    def __init__(self):
+        self.main_window =tkinter.Tk()
+
+        self.my_button1 = tkinter.Button(self.main_window, text='Processing Payroll')
+        self.my_button2 = tkinter.Button(self.main_window, text='View/Add Employees.')
+        self.my_button3 = tkinter.Button(self.main_window, text='View All Time Cards')
+        self.my_button4 = tkinter.Button(self.main_window, text='View Pay Rate/Salary For Employee')
+        self.my_button5 = tkinter.Button(self.main_window, text='View Sales For Employee')
+        self.my_button6 = tkinter.Button(self.main_window, text='Chane Payroll Payment Method')
+        self.my_button7 = tkinter.Button(self.main_window, text='Run Payroll')
+        self.quit_button = tkinter.Button(self.main_window, text='QUIT', command=self.main_window.destroy)
+
+        self.my_button1.pack()
+        self.my_button2.pack()
+        self.my_button3.pack()
+        self.my_button4.pack()
+        self.my_button5.pack()
+        self.my_button6.pack()
+        self.my_button7.pack()
+        self.quit_button.pack()
+
+        tkinter.mainloop()
+
+    # def do_something(self):
+
+    # create an instance of Process Payroll
+process_payroll = ProcessingPayroll()
