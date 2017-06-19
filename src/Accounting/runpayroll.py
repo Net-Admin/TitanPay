799b3d0cf6fdaf23b25eb0fa0de59116ec056a2f
twilhite82 <twilhite82@users.noreply.github.com>
2017-06-18 21:52:31 -0400

    Update runpayroll.py

--- src/Accounting/runpayroll.py
+++ src/Accounting/runpayroll.py
@@ -1,11 +1,12 @@
 import tkinter
+import tkinter.messagebox
 
 class RunningPayroll:
 
     def __init__(self):
         self.main_window =tkinter.Tk()
 
-        self.my_button1 = tkinter.Button(self.main_window, text='Running Payroll')
+        self.my_button1 = tkinter.Button(self.main_window, text='Run Payroll', command=self.do_something)
         self.quit_button = tkinter.Button(self.main_window, text='QUIT', command=self.main_window.destroy)
 
         self.my_button1.pack()
@@ -18,6 +19,7 @@ class RunningPayroll:
         tkinter.mainloop()
 
     # def do_something(self):
-
+    def do_something(self):
+        tkinter.messagebox.showinfo('Response', 'Thank You')
     # create an instance of Running Payroll
 run_payroll = RunningPayroll()
