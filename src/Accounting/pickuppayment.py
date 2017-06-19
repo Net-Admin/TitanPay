7dc655b1573dde5dc1b1075545ad0d943fd0a2e3
twilhite82 <twilhite82@users.noreply.github.com>
2017-06-19 19:17:26 -0400

    Update pickuppayment.py

--- src/Accounting/pickuppayment.py
+++ src/Accounting/pickuppayment.py
@@ -5,4 +5,4 @@ class PickUpPayment(PaymentMethod):
     def __init__(self):
 
     def pickup_payment(self, pay, Address):
-        return pickup_payment
+        return ("A Check for ", amt, " is waiting for ", self, " at the PostMaster."
