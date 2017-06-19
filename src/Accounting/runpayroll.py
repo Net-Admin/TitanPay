f4ccb1c8fd4ccc412b9b899e74476b22ea452e2f
twilhite82 <twilhite82@users.noreply.github.com>
2017-06-18 23:13:53 -0400

    Update runpayroll.py

--- src/Accounting/runpayroll.py
+++ src/Accounting/runpayroll.py
@@ -5,20 +5,29 @@ class RunningPayroll:
 
     def __init__(self):
         self.main_window =tkinter.Tk()
+        self.new_window = tkinter.Tk()
 
-        self.my_button1 = tkinter.Button(self.main_window, text='Run Payroll', command=self.do_something)
+        self.my_button1 = tkinter.Button(self.main_window, text='Process Payroll', command=self.run_payroll)
         self.quit_button = tkinter.Button(self.main_window, text='QUIT', command=self.main_window.destroy)
+        
 
         self.my_button1.pack()
         self.quit_button.pack()
 
-        employee_file = open(compiled_employees.txt', 'a')
-        timesheet_file = open(timesheet.txt', 'a')
-        payment_file = open(.txt', 'a')
+        employee_file = open('compiled_employees.txt', 'r')
+        timesheet_file = open('timesheet.txt', 'r')
+        payment_file = open('.txt', 'r')
 
         tkinter.mainloop()
 
     # def do_something(self):
+    def run_payroll(self):
+        self.my_button2 = tkinter.Button(self.new_window, text='Run Payroll', command=self.do_something)
+        self.quit_button2 = tkinter.Button(self.new_window, text='QUIT', command=self.new_window.destroy)
+                
+        self.my_button2.pack()
+        self.quit_button2.pack()
+                            
     def do_something(self):
         tkinter.messagebox.showinfo('Response', 'Thank You')
     # create an instance of Running Payroll
