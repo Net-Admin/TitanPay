0dedb010df576146d460701415c052f4c987478b
Joseph Parise <medic_24@hotmail.com>
2017-06-19 23:28:09 -0400

    6/19/2017 v5

--- src/Accounting/process_payroll.py
+++ src/Accounting/process_payroll.py
@@ -2,11 +2,9 @@ import tkinter
 
 class ProcessingPayroll:
 
-    def __init__(self):
+    def __init__(self, employee):
         self.main_window =tkinter.Tk()
-
-    def callpayroll():
-        execfile(compile(open("python runpayroll.py"))
+        self.employee = employee
 
         self.my_button1 = tkinter.Button(self.main_window, text='Processing Payroll')
         self.my_button2 = tkinter.Button(self.main_window, text='View/Add Employees.')
@@ -14,8 +12,7 @@ class ProcessingPayroll:
         self.my_button4 = tkinter.Button(self.main_window, text='View Pay Rate/Salary For Employee')
         self.my_button5 = tkinter.Button(self.main_window, text='View Sales For Employee')
         self.my_button6 = tkinter.Button(self.main_window, text='Change Payroll Payment Method')
-        #self.my_button7 = tkinter.Button(self.main_window, text='Run Payroll')
-        self.my_button7 = tkinter.Button(self.main_window, text='Run Payroll', command=callpayroll)
+        self.my_button7 = tkinter.Button(self.main_window, text='Run Payroll', command=self.run)
         self.quit_button = tkinter.Button(self.main_window, text='QUIT', command=self.main_window.destroy)
 
         self.my_button1.pack()
@@ -27,9 +24,27 @@ class ProcessingPayroll:
         self.my_button7.pack()
         self.quit_button.pack()
 
-        tkinter.mainloop()
+        timesheet_file = open('timecards.txt', 'r')
 
+    def run(self):
+            employee_list = []
+            infile = open(r'D:\Users\medic\PycharmProjects\TitanPay\src\Accounting\compiled_employees.txt', 'r')
+            file_contents = infile.read()
+            infile = open(r'D:\Users\medic\PycharmProjects\TitanPay\src\Accounting\timecards.txt', 'r')
+            file_contents = infile.read()
+
+            for count in range(1, 5):
+                global employee
+                employee = employee + 1
+                employee = employee.Employee(self, employee_id, clock_in, clock_out)
+                employee_list.append(employee)
+            return employee_list
+
+            pay = HourlyEmployees.get(self, pay)
+
+            tkinter.messagebox.showinfo('Response', 'self.payment_method')
     # def do_something(self):
+            tkinter.mainloop()
 
     # create an instance of Process Payroll
 process_payroll = ProcessingPayroll()
--- src/Accounting/runpayroll.py
+++ src/Accounting/runpayroll.py
@@ -3,9 +3,10 @@ import tkinter.messagebox
 
 class RunningPayroll:
 
-    def __init__(self):
+    def __init__(self, employee):
         self.main_window =tkinter.Tk()
         self.new_window = tkinter.Tk()
+        self.employee = employee
 
         self.my_button1 = tkinter.Button(self.main_window, text='Process Payroll', command=self.run_payroll)
         self.quit_button = tkinter.Button(self.main_window, text='QUIT', command=self.main_window.destroy)
@@ -29,16 +30,18 @@ class RunningPayroll:
         self.quit_button2.pack()
                             
     def run(self):
-        employees_list = []
+        employee_list = []
         infile = open(r'D:\Users\medic\PycharmProjects\TitanPay\src\Accounting\compiled_employees.txt', 'r')
         file_contents = infile.read()
         infile = open(r'D:\Users\medic\PycharmProjects\TitanPay\src\Accounting\timecards.txt', 'r')
-        file_contents - infile.read()
+        file_contents = infile.read()
         
-        for count in range(employees_list):
-           employees = employees.Employees(self, employee_id, clock_in, clock_out)
-           employees_list.append(employees)
-        return employees_list
+        for count in range(1,5):
+           global employee
+           employee = employee+1
+           employee = employee.Employee(self, employee_id, clock_in, clock_out)
+           employee_list.append(employee)
+        return employee_list
     
         pay = HourlyEmployees.get(self, pay)
     
