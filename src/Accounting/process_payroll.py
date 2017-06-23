c666f8b50ec14c3bd15823ca5325cb83ac4ed377
Joseph Parise <medic_24@hotmail.com>
2017-06-22 22:38:05 -0400

    6/22/2017 v4

--- src/Accounting/process_payroll.py
+++ src/Accounting/process_payroll.py
@@ -9,17 +9,16 @@ class ProcessingPayroll:
 
     def __init__(self):
         self.main_window =tkinter.Tk()
-
-        self.my_button1 = tkinter.Button(self.main_window, text='Processing Payroll')
+        self.label1 = tkinter.Label(self.main_window, text='Processing Payroll')
         self.my_button2 = tkinter.Button(self.main_window, text='View/Add Employees.')
-        self.my_button3 = tkinter.Button(self.main_window, text='View All Time Cards')
-        self.my_button4 = tkinter.Button(self.main_window, text='View Pay Rate/Salary For Employee')
-        self.my_button5 = tkinter.Button(self.main_window, text='View Sales For Employee')
-        self.my_button6 = tkinter.Button(self.main_window, text='Change Payroll Payment Method', command=payroll)
-        self.my_button7 = tkinter.Button(self.main_window, text='Run Payroll')
+        self.my_button3 = tkinter.Button(self.main_window, text='View All Time Cards For Given Employees')
+        self.my_button4 = tkinter.Button(self.main_window, text='View Pay Rate/Salary Information For A Given Employee')
+        self.my_button5 = tkinter.Button(self.main_window, text='View Sales For Salaried Employee')
+        self.my_button6 = tkinter.Button(self.main_window, text='Change A Given Employees Payroll Payment Method', command=payroll)
+        self.my_button7 = tkinter.Button(self.main_window, text='Run Payroll And View Results')
         self.quit_button = tkinter.Button(self.main_window, text='QUIT', command=self.main_window.destroy)
 
-        self.my_button1.pack()
+        self.label1.pack()
         self.my_button2.pack()
         self.my_button3.pack()
         self.my_button4.pack()
