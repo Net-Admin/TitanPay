e3a1d650620573e846269ce8418756425183bd46
twilhite82 <twilhite82@users.noreply.github.com>
2017-06-28 22:25:20 -0400

    Update runpayroll.py

--- src/Accounting/runpayroll.py
+++ src/Accounting/runpayroll.py
@@ -7,9 +7,6 @@ import datetime
 
 class Payroll:
 
-    def __init__(self):
-        self.employees = []
-
     def run(self):
         self.my_button2 = tkinter.Button(self.new_window, text='Run Payroll', command=self.run)
         self.quit_button2 = tkinter.Button(self.new_window, text='QUIT', command=self.new_window.destroy)
@@ -42,17 +39,17 @@ class Payroll:
                 
         while hr_contents != '':
            employee = hr_contents.split(',')
-           employee_id = int(employee[0])
-           last_name = int(employee[1])
-           first_name = int(employee[2])
-           hourlyrate = int(employee[3])
-           uniondues = int(employee[4])
-           payment_method = int(employee[5])
+           employee_id = row(employee[0])
+           last_name = row(employee[1])
+           first_name = row(employee[2])
+           hourlyrate = row(employee[3])
+           uniondues = row(employee[4])
+           payment_method = row(employee[5])
            while t_contents != "":
                time = t_contents.split(',')
                if int(time[0]) == employee_id:
-                   clock_in = int(time[1])
-                   clock_out = int(time[2])
+                   clock_in = row(time[1])
+                   clock_out = row(time[2])
                    employee = employee.hourlyemployee(self, employee_id, first_name, last_name, hourlyrate, uniondues, payment_method)
                    employee_list.append(employee)
                t_contents = tinfile.readline()
@@ -60,18 +57,18 @@ class Payroll:
 
         while sal_contents != '':
             employ = sal_contents.split(',')
-            employee_id = int(employ[0])
-            last_name = int(employee(1))
-            first_name = int(employee(2))
-            salary = int(employee(3))
-            commissionrate = int(employee(4))
-            uniondues = int(employee(5))
-            payment_method = int(employee(6))
+            employee_id = row(employ[0])
+            last_name = row(employee(1))
+            first_name = row(employee(2))
+            salary = row(employee(3))
+            commissionrate = row(employee(4))
+            uniondues = row(employee(5))
+            payment_method = row(employee(6))
             salemployee = employee.salariedemployee(self, employee_id, first_name, last_name, salary, commissionrate, uniondues, payment_method)
             employee_list.append(salemployee)
             while rec_contents != "":
                 receipt = rec_contents.split(',')
-                if int(receipt[0]) == employee_id:
+                if row(receipt[0]) == employee_id:
                     rec_contents = recfile.readline()
 
         pay = HourlyEmployees.get(self, pay)
