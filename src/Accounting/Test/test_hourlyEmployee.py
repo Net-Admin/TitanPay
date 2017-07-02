31b112828fc73dceab35ecfe146246a52ec41037
Joseph Parise <medic_24@hotmail.com>
2017-07-02 14:30:42 -0400

    cleanup
    moved test files to Test folder,
    moved data files to Data folder,
    moved db files to DataBase folder

--- src/Accounting/Test/test_hourlyEmployee.py
+++ src/Accounting/Test/test_hourlyEmployee.py
@@ -1,26 +1,12 @@
 from unittest import TestCase
 
-class TestHourlyEmployee(TestCase):
-    def __init__(self):
-        self.__timecards
 
-       def test_clockIn(self):
-        hourly = HourlyEmployee('10', 'Chris', 'Platt', '999', '150', 'DD')
-        hourly.clock_in()
-        timecard = hourly.get_time_card()
-        self.assertIsInstance(timecard, TimeCard)
+class TestHourlyEmployee(TestCase):
+    def test_clockIn(self):
+        self.fail()
 
     def test_clockOut(self):
-        hourly = HourlyEmployee('10', 'Chris', 'Platt', '999', '150', 'DD')
-        hourly.clock_in()
-        hourly.clock_out()
-        time = TimeCard.calculate_daily_pay()
-        self.assertTrue(time >= 1)
+        self.fail()
 
     def test_calculate_pay__(self):
-        hourly = HourlyEmployee('10', 'Chris', 'Platt', '999', '150', 'DD')
-        hourly.clock_in()
-        hourly.clock_out()
-        time = TimeCard.calculate_daily_pay()
-        self.assertTrue(time >= 1)
-
+        self.fail()
